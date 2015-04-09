83d5e6bac334dad1ab63183b1e9e654869d6d298
Tara Keena <tarakeena12@gmail.com>
2015-04-09 16:06:15 +0100

    first commit on learning file

---
+++ src/Bike.java
@@ -0,0 +1,23 @@
+
+public class Bike {
+	
+	private String ownerName;
+	
+	public Bike()
+	{
+		
+	}
+	
+	public String getOwnerName()
+	{
+		return ownerName;
+	}
+	
+	public void setOwnerName(String name)
+	{
+		ownerName = name;
+	}
+	
+	
+
+}
---
+++ src/Learning2.java
@@ -0,0 +1,16 @@
+
+public class Learning2 {
+	
+	public static void main(String [] args)
+	
+	{
+		Bike b1 = new Bike();
+		String name;
+		b1.setOwnerName("Tara");
+		
+		name = b1.getOwnerName();
+		System.out.print(name);
+		
+	}
+
+}
